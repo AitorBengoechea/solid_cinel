b52d6aafe5265ef275b62bdc9b38390723575436
AitorBengoechea <90201642+AitorBengoechea@users.noreply.github.com>
2022-10-27 14:34:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
+import os 
 setup(
       name='solid_cinel',
       version='0.1',
       description='solid_cinel',
-      url='https://github.com/AitorBengoechea/solid_cinel',
+      url='https://github.com/AitorBengoechea/solid_cinel/',
       author='Aitor Bengoechea',
       author_email='aitorabf@gmail.com',
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: Beta',
           'Programming Language :: Python :: 3',
           ],
-      #keywords=", ".join(keywords),
-      # packages = find_packages(),
       data_files=[(x[0], list(map(lambda y: x[0]+'/'+y, x[2]))) for x in os.walk('solid_cinel')],
       #install_requires=open(requirements).read().splitlines(),
       zip_safe=False,
@@ -23,6 +22,6 @@ setup(
       #entry_points={
       #    'console_scripts': [
       #        'sandy=sandy.sampling:run',
-      #        ],
+      #       ],
       #    },
       )

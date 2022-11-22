361c5de94c9042ccfb7bf2f64b1630487e42b4eb
BENGOECHEA FERNANDEZ Aitor 272525 <aitorabf@gmail.com>
2022-11-22 14:29:00 +0100

    update

--- solid_cinel/tests/core/material/test_hkloop_minimization.py
+++ solid_cinel/tests/core/material/test_hkloop_minimization.py
@@ -5,15 +5,21 @@ Created on Mon Nov 21 14:13:08 2022
 @author: AB272525
 """
 
-from solid_cinel.core.material.crystal_symmetry import dir_vector_operator
-from solid_cinel.core.material.solid import Solid, hkl_minimize
+from solid_cinel.core.material.crystal_symmetry import Crystal_structure
+from solid_cinel.core.material.solid import Solid, hkl_max_value
 import numba as nb
 import numpy as np
 import pandas as pd
+import platform
 import pytest
 
+if platform.system().lower() == "linux":
+    parallel = True
+else:
+    parallel = False
 
-@nb.jit(nopython=True, nogil=True)
+
+@nb.jit(nopython=True, nogil=True, parallel=parallel)
 def hklloop_max(rec_vecs, d_min):
     hklM = []
     hklrange = np.arange(-100, 100 + 1)
@@ -36,21 +42,15 @@ def hklloop_max(rec_vecs, d_min):
 
 @pytest.mark.parametrize("d_min", [0.2360746677309732])
 def test_hklloop_aprox(d_min):
-    preferred_orientation = np.array([ 0, 1, 1 ])
     dir_vec_length = np.random.rand(3) * 4
-    dir_vec_angles = np.random.rand(3) * 180 
-    unit_pos = np.array([0.25, 0.25, 0.25, 0.75, 0.25, 0.25, 0.25, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.25, 0.75, 0.25, 0.25, 0.75, 0.75, 0.75, 0.25, 0.25,0.75, 0.25])
-    A = 27
-    Z = 13
-    atomic_mass_Al27 = 26.98153433356103
-    b_coh_Al27  = 3.449
-    b_incoh_Al27 = 0.256
-    Al = Solid(dir_vec_length, dir_vec_angles, preferred_orientation, unit_pos, A, Z, atomic_mass_Al27, b_coh_Al27, b_incoh_Al27)
-    rec_vecs = Al.reciproc_vec
-    if rec_vecs.isnull().values.any():
-        assert True == True
-    else:
-        rec_vecs = rec_vecs.values
-        full_hkl = hklloop_max(rec_vecs, d_min).max(axis=0)
-        hkl_minimization = hkl_minimize(rec_vecs, d_min)
-        assert (hkl_minimization >= full_hkl).all() == True
+    dir_vec_angles = np.random.rand(3) * 180
+    crys = Crystal_structure(dir_vec_length, dir_vec_angles)
+    rec_vecs = crys.reciproc_vec
+    while rec_vecs.isnull().values.any():
+        dir_vec_angles = np.random.rand(3) * 180
+        crys = Crystal_structure(dir_vec_length, dir_vec_angles)
+        rec_vecs = crys.reciproc_vec
+    rec_vecs = rec_vecs.values
+    full_hkl = hklloop_max(rec_vecs, d_min).max(axis=0)
+    hkl_minimization = hkl_max_value(rec_vecs, d_min)
+    assert (hkl_minimization >= full_hkl).all()

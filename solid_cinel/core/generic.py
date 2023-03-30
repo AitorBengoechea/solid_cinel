10b326e6a9569dc9357d2716a4fd41ff23120de2
Aitor <aitorabf@gmail.com>
2023-03-30 10:27:42 +0200

    generic.py file docstring change

--- solid_cinel/core/generic.py
+++ solid_cinel/core/generic.py
@@ -6,24 +6,29 @@ Created on Tue Nov 15 09:11:55 2022
 """
 import scipy as sp
 import pandas as pd
-import numpy as np
+from collections.abc import Iterable
 
 
-def integrate(series, kind="trapezoidal") -> float:
+def integrate(series: pd.Series, kind="trapezoidal") -> float:
     """
     Get normalization coefficient of a function.
 
     Parameters
     ----------
-    series : TYPE
+    series : "pd.Series"
         Function to test if it is normlize.
     kind : "str", optional
         integral type. The default is "trapezoidal".
 
+    Returns
+    -------
+    "float"
+        The integration value.
+
     Raises
     ------
     ValueError
-        Kind is not available in github.
+        Kind is not available in scipy.
 
     Example
     -------
@@ -42,7 +47,8 @@ def integrate(series, kind="trapezoidal") -> float:
     return y_norm
 
 
-def reshape_differential(x, y, xnew, kind="slinear", bounds_error=False):
+def reshape_differential(x: Iterable[:], y: Iterable[:], xnew: Iterable[:],
+                         kind: str="slinear", bounds_error: bool=False):
     """
     Linearly interpolate array over new energy grid structure.
     Extrapolated values are replaced by zeros.
@@ -55,6 +61,22 @@ def reshape_differential(x, y, xnew, kind="slinear", bounds_error=False):
         new energy grid
     y : `numpy.ndarray` with at least two entries and same length as `x`
         array to interpolate
+    kind: "str"
+        Specifies the kind of interpolation as a string or as an integer
+        specifying the order of the spline interpolator to use. The string has
+        to be one of ‘linear’, ‘nearest’, ‘nearest-up’, ‘zero’, ‘slinear’,
+        ‘quadratic’, ‘cubic’, ‘previous’, or ‘next’. ‘zero’, ‘slinear’,
+        ‘quadratic’ and ‘cubic’ refer to a spline interpolation of zeroth,
+        first, second or third order; ‘previous’ and ‘next’ simply return the
+        previous or next value of the point; ‘nearest-up’ and ‘nearest’ differ
+        when interpolating half-integers (e.g. 0.5, 1.5) in that ‘nearest-up’
+        rounds up and ‘nearest’ rounds down. Default is ‘linear’
+    bounds_error: "bool"
+        if True, a ValueError is raised any time interpolation is attempted on a
+        value outside of the range of x (where extrapolation is necessary). If
+        False, out of bounds values are assigned fill_value. By default, an
+        error is raised unless fill_value="extrapolate".
+
     Returns
     -------
     `numpy.ndarray` with length `len(xnew)`

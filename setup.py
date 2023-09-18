2bc8480419fedd9b6800a90d6ac3f743d3bea29c
Aitor <aitorabf@gmail.com>
2023-09-18 10:02:52 +0200

    setup change the installation

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
 import os
-from numpy.distutils.core import setup
+from setuptools import setup
+import multiprocessing
 requirements = "requirements.txt"
-setup(
-      name='solid_cinel',
-      version='0.1',
-      description='solid_cinel',
-      url='https://github.com/AitorBengoechea/solid_cinel/',
-      author='Aitor Bengoechea',
-      author_email='aitorabf@gmail.com',
-      classifiers=[
-          'Development Status :: Beta',
-          'Programming Language :: Python :: 3',
-          ],
-      data_files=[(x[0], list(map(lambda y: x[0]+'/'+y, x[2]))) for x in os.walk('solid_cinel')],
-      install_requires=open(requirements).read().splitlines(),
-      zip_safe=False,
-      # setup_requires=["pytest-runner",],
-      tests_require=[
-          "pytest",
-          ],
-      include_package_data=True,
-      #ext_modules=extensions,
-      #entry_points={
-      #    'console_scripts': [
-      #        'sandy=sandy.sampling:run',
-      #       ],
-      #    },
-      )
+
+if __name__ == "__main__":
+    # Freeze to support parallel compilation when using spawn instead of fork
+    multiprocessing.freeze_support()
+    setup(
+        name='solid_cinel',
+        version='0.1',
+        description='solid_cinel',
+        url='https://github.com/AitorBengoechea/solid_cinel/',
+        author='Aitor Bengoechea',
+        author_email='aitorabf@gmail.com',
+        classifiers=[
+            'Development Status :: Beta',
+            'Programming Language :: Python :: 3',
+        ],
+        data_files=[(x[0], list(map(lambda y: x[0] + '/' + y, x[2]))) for x in
+                    os.walk('solid_cinel')],
+        install_requires=open(requirements).read().splitlines(),
+        zip_safe=False,
+        # setup_requires=["pytest-runner",],
+        tests_require=[
+            "pytest",
+        ],
+        include_package_data=True,
+        # ext_modules=extensions,
+        # entry_points={
+        #    'console_scripts': [
+        #        'sandy=sandy.sampling:run',
+        #       ],
+        #    },
+    )

3c7631e43260887e52e92c205edc36101c1b44ce
aitor <aitorabf@gmail.com>
2023-12-20 10:58:36 +0100

    github workflow instalation

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 from setuptools import setup, find_packages
 import multiprocessing
-import subprocess
 
 requirements_file = "requirements.txt"
-
-# Check for CUDA JIT
-try:
-    result = subprocess.run(['nvcc', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if 'release' in result.stdout.decode('utf-8'):
-        with open(requirements_file, 'a') as f:
-            f.write('\ncupy\n')
-except FileNotFoundError:
-    pass
 
 if __name__ == "__main__":
     # Freeze to support parallel compilation when using spawn instead of fork

aed816465685d22f3e6a230436e970a990dc3904
aitor <aitorabf@gmail.com>
2024-01-19 22:00:07 +0100

    Erase repeated code

--- solid_cinel/core/material/vibration/tau.py
+++ solid_cinel/core/material/vibration/tau.py
@@ -37,7 +37,7 @@ def optional_jit(func):
 
 
 @optional_jit
-def tau_n_convolution(delta_beta, tau1, tau_n_minus_1, i):
+def tau_n_convolution(delta_beta, tau1, Ntau1, tau_n_minus_1, Ntaunm1, i):
     """
     Calculate the convolution in the tau_n[i] between  tau1 and tau_n_minus_1.
 
@@ -47,8 +47,12 @@ def tau_n_convolution(delta_beta, tau1, tau_n_minus_1, i):
         beta interval between two consecutive values.
     tau1: np.ndarray, (N,)
         Tau(-beta) function values for n = 1 expansion.
+    Ntau1: int
+        Length of tau1.
     tau_n_minus_1: np.ndarray, (M,)
         Tau(-beta) function values for n-1 expansion.
+    Ntaunm1: int
+        Length of tau_n_minus_1.
     i: int
         Index of the tau_n[i] value to calculate.
 
@@ -57,24 +61,23 @@ def tau_n_convolution(delta_beta, tau1, tau_n_minus_1, i):
     convol: float
         Convolution value.
     """
-    N = len(tau1)
-    Nnm1 = len(tau_n_minus_1)  # length of tau_n_minus_1
+    trapz_limit = Ntau1 - 1
     convol = 0.
-    for j in range(1, N):
+    for j in range(1, Ntau1):
         convol_j = 0.
 
         k = i - j  # tau_n_minus_1(-(beta-beta^prime))
-        if abs(k) < Nnm1:
+        if abs(k) < Ntaunm1:
             if k >= 0:
                 convol_j += tau_n_minus_1[k]
             else:
                 convol_j += tau_n_minus_1[-k] * exp(k * delta_beta)
 
         l = i + j  # Tau_n_minus_1(-(beta+beta^prime))
-        if l < Nnm1:
+        if l < Ntaunm1:
             convol_j += tau_n_minus_1[l] * exp(-j * delta_beta)
 
-        if j == N - 1:
+        if j == trapz_limit:
             convol_j *= 0.5  # trapz integrate
 
         convol += tau1[j] * convol_j * delta_beta
@@ -83,8 +86,8 @@ def tau_n_convolution(delta_beta, tau1, tau_n_minus_1, i):
 
 
 @optional_jit
-def calculate_tau_n(delta_beta, tau1, tau_n_minus_1, tau_n,
-                    start, final, stride):
+def calculate_tau_n(delta_beta, tau1, Ntau1, tau_n_minus_1, tau_n,
+                    start, Ntaun, stride):
     """
     Calculate the tau_n(-beta) function values for all n. The values are
     calculated until the last value of tau_n is zero.
@@ -111,18 +114,18 @@ def calculate_tau_n(delta_beta, tau1, tau_n_minus_1, tau_n,
     tau_n: np.ndarray, (M,)
         Tau(-beta) function values for n expansion.
     """
-    Nnm1 = len(tau_n_minus_1)
+    Ntaunm1 = Ntaun - Ntau1 + 1
     # Tau_N(-beta) loop:
-    for i in range(start, final, stride):
+    for i in range(start, Ntaun, stride):
         # 1 iteration: j = 0
-        tau_n[i] += tau1[0] * tau_n_minus_1[i] * delta_beta if i < Nnm1 else 0.
+        tau_n[i] += tau1[0] * tau_n_minus_1[i] * delta_beta if i < Ntaunm1 else 0.
         # rest of iterations:
-        tau_n[i] += tau_n_convolution(delta_beta, tau1, tau_n_minus_1, i)
+        tau_n[i] += tau_n_convolution(delta_beta, tau1, Ntau1, tau_n_minus_1, Ntaunm1,  i)
 
 
 @cuda.jit
-def tau_n_calculation_threads(delta_beta, tau1, tau_n_minus_1, tau_n_device,
-                              final):
+def tau_n_calculation_threads(delta_beta, tau1, Ntau1, tau_n_minus_1, tau_n_device,
+                              Ntaun):
     """
     Calculate the tau_n(-beta) function values for all n. The values are
     calculated until the last value of tau_n is zero.
@@ -147,24 +150,30 @@ def tau_n_calculation_threads(delta_beta, tau1, tau_n_minus_1, tau_n_device,
     """
     start = cuda.grid(1)
     stride = cuda.gridsize(1)
-    calculate_tau_n(delta_beta, tau1, tau_n_minus_1, tau_n_device,
-                    start, final, stride)
+    calculate_tau_n(delta_beta, tau1, Ntau1, tau_n_minus_1, tau_n_device,
+                    start, Ntaun, stride)
 
 
 @nb.jit(nopython=True, nogil=True, cache=True, parallel=False)
-def tau_n_functions_cpu(tau1: np.ndarray, delta_beta: float,
-                        nphonon: int, threshold: float):
+def calculate_tau_n_functions_cpu(tau_n_func: np.ndarray, tau1: np.ndarray,
+                                  Ntau1: int, delta_beta: float, nphonon: int, Ntaun: int,):
     """
     Get the tau_{n}(-beta) function values for all n.
 
     Parameters
     ----------
+    tau_n_func: 'np.ndarray', (nphonon, N * nphonon)
+        All Tau(-beta) function values for n = 1 expansion.
     tau1: 'np.ndarray', (N,)
         Tau(-beta) function values for n = 1 expansion.
+    Ntau1: int
+        Length of tau1.
     delta_beta: 'float'
         Interval of beta for the PDOS.
     nphonon: 'int'
         Number of phonon to calculate the tau functions.
+    Ntaun: 'int'
+        Length of the tau_2 function: 2 * N - 1.
     threshold: 'float'
         Minimun value to take into account.
 
@@ -173,43 +182,43 @@ def tau_n_functions_cpu(tau1: np.ndarray, delta_beta: float,
     tau_n_func: 'np.ndarray', (N * nphonon, nphonon)
         All Tau(-beta) function values for n expansion.
     """
-    tau_n_func = np.zeros((nphonon, len(tau1) * nphonon))
-    tau_n_func[0, :len(tau1)] += tau1
     tau_n_minus_1 = tau1.copy()
-    N = len(tau1)
-    Ntau = 2 * N - 1
     for n in range(1, nphonon):
-        tau_n = np.zeros(Ntau)
-        calculate_tau_n(delta_beta, tau1, tau_n_minus_1, tau_n, 0, Ntau, 1)
+        tau_n = np.zeros(Ntaun)
+        calculate_tau_n(delta_beta, tau1, Ntau1, tau_n_minus_1, tau_n, 0, Ntaun, 1)
         # Copy thet data into the array:
-        tau_n_func[n, :Ntau] += tau_n
+        tau_n_func[n, :Ntaun] += tau_n
         # If the last N values are zero, the next tau_n will have the same length
         # because the convolution will be zero for the following values
-        Ntau = Ntau if np.all(tau_n[-N:] == 0.0) else Ntau + N - 1
+        Ntaun = Ntaun if np.all(tau_n[-Ntau1:] == 0.0) else Ntaun + Ntau1 - 1
         # Next tau_n:
         tau_n_minus_1 = tau_n
 
     # Erase the zeros in the last part of the array
-    if threshold == 0.0:
-        return tau_n_func[::, :Ntau]
-    else:
-        return tau_n_func[::, :first_all_zero_column(tau_n_func, threshold)]
+    return tau_n_func[::, :Ntaun]
 
 
-def tau_n_functions_gpu(tau1: np.ndarray, delta_beta: float,
-                        nphonon: int, threshold: float,
-                        threadsperblock: int = 128):
+def calculate_tau_n_functions_gpu(tau_n_func: np.ndarray, tau1: np.ndarray,
+                                  Ntau1: int, delta_beta: float, nphonon: int, Ntaun: int,
+                                  threadsperblock: int = 128) -> np.ndarray:
+
     """
     Get the tau_{n}(-beta) function values for all n.
 
     Parameters
     ----------
+    tau_n_func: 'np.ndarray', (nphonon, N * nphonon)
+        All Tau(-beta) function values for n = 1 expansion.
     tau1: 'np.ndarray', (N,)
         Tau(-beta) function values for n = 1 expansion.
+    Ntau1: int
+        Length of tau1.
     delta_beta: 'float'
         Interval of beta for the PDOS.
     nphonon: 'int'
         Number of phonon to calculate the tau functions.
+    Ntaun: 'int'
+        Length of the tau_2 function: 2 * N - 1.
     threshold: 'float'
         Minimun value to take into account.
     threadsperblock: 'int'
@@ -221,37 +230,40 @@ def tau_n_functions_gpu(tau1: np.ndarray, delta_beta: float,
     tau_n_func: 'np.ndarray', (N * nphonon, nphonon)
         All Tau(-beta) function values for n expansion.
     """
-    tau_n_func = np.zeros((nphonon, len(tau1) * nphonon))
-    tau_n_func[0, :len(tau1)] += tau1
-    N = len(tau1)
-    Ntau = 2 * N - 1
     # Copy the data to the device
     tau1 = cuda.to_device(tau1)
     tau_n_minus_1 = cuda.to_device(tau1)
     for n in range(1, nphonon):
         # Perform the calculation on the device:
-        tau_n = cuda.to_device(np.zeros(Ntau))
-        blockspergrid = Ntau + threadsperblock - 1
+        tau_n = cuda.to_device(np.zeros(Ntaun))
+        blockspergrid = Ntaun + threadsperblock - 1
         blockspergrid //= threadsperblock
-        tau_n_calculation_threads[blockspergrid, threadsperblock](delta_beta, tau1, tau_n_minus_1,
-                                                                  tau_n, Ntau)
+        tau_n_calculation_threads[blockspergrid, threadsperblock](delta_beta, tau1, Ntau1, tau_n_minus_1,
+                                                                  tau_n, Ntaun)
 
         # Copy the data back to the host
-        tau_n_func[n, :Ntau] += tau_n.copy_to_host()
+        tau_n_func[n, :Ntaun] += tau_n.copy_to_host()
 
         # If the last N values are zero, the next tau_n will have the same length
         # because the convolution will be zero for the following values
-        Ntau = Ntau if np.all(tau_n[-N:] == 0.0) else Ntau + N - 1
+        Ntaun = Ntaun if np.all(tau_n[-Ntau1:] == 0.0) else Ntaun + Ntau1 - 1
         # Next tau_n:
         tau_n_minus_1 = tau_n
+    return tau_n_func[::, :Ntaun]
+
+
+def tau_n_functions(tau1: np.ndarray, delta_beta: float,
+                    nphonon: int, threshold: float):
+    Ntau1 = len(tau1)
+    column_max = Ntau1 * nphonon
+    tau_n_func = np.zeros((nphonon, column_max))
+    tau_n_func[0, :Ntau1] += tau1
+    calculate_tau_n_functions = calculate_tau_n_functions_gpu if gpu_available else calculate_tau_n_functions_cpu
+    tau_n_func = calculate_tau_n_functions(tau_n_func, tau1, Ntau1, delta_beta, nphonon, 2 * Ntau1 - 1)
     # Erase the zeros in the last part of the array
-    if threshold == 0.0:
-        return tau_n_func[::, :Ntau]
-    else:
-        return tau_n_func[::, :first_all_zero_column(tau_n_func, threshold)]
-
-
-tau_n_functions = tau_n_functions_gpu if gpu_available else tau_n_functions_cpu
+    if threshold > 0.0:
+        column_max = first_all_zero_column(tau_n_func, threshold)
+    return tau_n_func[::, :column_max]
 
 
 def save_tau(tau_n: np.ndarray, nphonon: int, T: float, tau_to_file: bool,

cda478e588649b5aa1b1bd0bef112f6be643e26c
aitor <aitorabf@gmail.com>
2024-06-29 13:43:58 +0200

    Optioanl argumentes not well writen

--- solid_cinel/application/teff.py
+++ solid_cinel/application/teff.py
@@ -4,19 +4,20 @@ from solid_cinel.core.material.vibration.pdos import Tpdos, Epdos
 
 
 def add_TeffArgs(parser: argparse.ArgumentParser):
+    # Required arguments for the calculation of the effective temperature:
     parser.add_argument('T', type=float,
                         help='Temperature in Kelvin')
     parser.add_argument('rho', type=str,
                         help='pdos file or values file')
-    parser.add_argument('dE', type=float, default=None,
-                        help='dE (optional, must be provided with values file)')
 
-    # Optional arguments for pd.read_csv:
-    parser.add_argument("header", type=int, default=None)
-    parser.add_argument("usecols", type=list, default=[0, 1])
-    parser.add_argument("index_col", type=int, default=0)
-    parser.add_argument("engine", type=str, default='python')
-    parser.add_argument("grid", type=str, default="dE")
+    # Optional arguments for the reading of the pdos file:
+    parser.add_argument('-dE', type=float, default=None,
+                        help='dE (optional, must be provided with values file)')
+    parser.add_argument("-header", type=int, default=None)
+    parser.add_argument("-usecols", type=list, default=[0, 1])
+    parser.add_argument("-index_col", type=int, default=0)
+    parser.add_argument("-engine", type=str, default='python')
+    parser.add_argument("-grid", type=str, default="dE")
 
 
 def handle_TeffArgs(args: argparse.Namespace) -> np.array:

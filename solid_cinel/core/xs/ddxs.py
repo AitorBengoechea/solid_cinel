9a9d6c99e27f62667eaa3c885dd4011ba3a4aadc
aitor <aitorabf@gmail.com>
2025-08-19 17:03:14 +0200

    1 version of update in ddxs

--- solid_cinel/core/xs/ddxs.py
+++ solid_cinel/core/xs/ddxs.py
@@ -80,13 +80,13 @@ class DDxs(DoubleDiffData):
         >>> theta = np.arange(0, 180, 1)[1::]
 
         # Angular distribution:
-        >>> DDxs.from_Sab(xs, Ein, T, Eout, theta, model="fgm").scatFunc.iloc[::200].round(6)
+        >>> DDxs.from_4PCF(xs, Ein, T, Eout, theta, model="fgm").scatFunc.iloc[::200].round(6)
         Eout
-        1.80000     0.768794
-        1.88008    10.451361
-        1.96016    54.522950
-        2.04024    34.506930
-        2.12032     2.920481
+        1.80000     0.766980
+        1.88008    10.435716
+        1.96016    54.493213
+        2.04024    34.519331
+        2.12032     2.924035
         dtype: float64
         """
         return super().columsIntegral
@@ -116,22 +116,21 @@ class DDxs(DoubleDiffData):
         >>> Ein = 2.0
         >>> Eout = np.linspace(Ein * 0.9 , Ein * 1.1, 1000)
         >>> theta = np.arange(0, 180, 15)[1::]
-        >>> ddxs = DDxs.from_Sab(xs, Ein, T, Eout, theta)
+        >>> ddxs = DDxs.from_4PCF(xs, Ein, T, Eout, theta)
         >>> ddxs.angularDistr.round(6)
         mu
-        -9.659258e-01    4.455539
-        -8.660254e-01    4.469560
-        -7.071068e-01    4.489373
-        -5.000000e-01    4.510598
-        -2.588190e-01    4.529192
-         6.123234e-17    4.543335
-         2.588190e-01    4.554042
-         5.000000e-01    4.563068
-         7.071068e-01    4.570756
-         8.660254e-01    4.576664
-         9.659258e-01    4.580387
+        -9.659258e-01    4.452497
+        -8.660254e-01    4.467374
+        -7.071068e-01    4.487391
+        -5.000000e-01    4.508718
+        -2.588190e-01    4.527563
+         6.123234e-17    4.542064
+         2.588190e-01    4.553174
+         5.000000e-01    4.562597
+         7.071068e-01    4.570632
+         8.660254e-01    4.576807
+         9.659258e-01    4.580694
         dtype: float64
-
         """
         return super().rowIntegral
 
@@ -164,7 +163,7 @@ class DDxs(DoubleDiffData):
         >>> pdos = Pdos.from_dE(rho_in_energy_U238, interv_in_energy_U238)
 
         # S(alpha, -beta) algorithm for FGM:
-        >>> float(round(DDxs.from_Sab(xs, Ein, T, Eout, theta, model="fgm").angleIntegrated, 2))
+        >>> float(round(DDxs.from_4PCF(xs, Ein, T, Eout, theta, model="fgm").angleIntegrated, 2))
         9.07
         """
         return super().doubleIntegral
@@ -193,8 +192,9 @@ class DDxs(DoubleDiffData):
         >>> Ein = 2.0
         >>> Eout = np.linspace(Ein * 0.9 , Ein * 1.1, 1000)
         >>> theta = np.arange(0, 180, 15)[1::]
-        >>> ddxs = DDxs.from_Sab(xs, Ein, T, Eout, theta)
-        >>> assert round(ddxs.upscattering, 6) == 0.389484
+        >>> ddxs = DDxs.from_4PCF(xs, Ein, T, Eout, theta)
+        >>> round(ddxs.upscattering, 2)
+        0.39
         """
         return integrate(super().columsPdf[self.Eout > self.Ein])
 
@@ -223,110 +223,15 @@ class DDxs(DoubleDiffData):
         >>> Ein = 2.0
         >>> Eout = np.linspace(Ein * 0.9 , Ein * 1.1, 1000)
         >>> theta = np.arange(0, 180, 15)[1::]
-        >>> ddxs = DDxs.from_Sab(xs, Ein, T, Eout, theta)
-        >>> assert round(ddxs.downscattering, 6) == 0.60678
+        >>> ddxs = DDxs.from_4PCF(xs, Ein, T, Eout, theta)
+        >>> round(ddxs.downscattering, 2)
+        0.61
         """
         return integrate(super().columsPdf[self.Eout < self.Ein])
 
     @classmethod
-    def from_Sab(cls, xs0K: Xs0K, Ein: float, T: float, Eout: np.ndarray, theta: np.ndarray, *args,
-                 **kwargs):
-        """
-        Generate the Double Differential XS for elastic scattering from
-        S(alpha, -beta) tables
-        ..math::
-            \frac{d^2\sigma_T(E)}{dE^\prime d^\theta} = \frac{\sigma_b}{2 * k_B * T}\sqrt{\frac{E^\prime}{E}} S(\alpha(\theta, E^\prime, E, M, T), \beta( E^\prime, E, T))
-
-        Common Parameters for fgm, sct and pdos models
-        ----------------------------------------------
-        xs0K: Xs0K
-            Xs object with the cross section xs data for the given material in barns
-        Ein : float
-        The incident energy of the neutron in eV
-        T : float
-            Temperature of the material in K
-        Eout : np.ndarray, (N,)
-            The neutron outgoing energy grid in eV
-        theta : np.ndarray, (M,)
-            The neutron outgoing angle grid in degrees (0, 180]
-
-        Parameters for sct
-        ------------------
-        pdos : 'solid_cinel.core.material.Pdos'
-            Pdos object.
-
-        Parameters for pdos
-        -------------------
-        pdos : 'solid_cinel.core.material.Pdos'
-            Pdos object.
-        threshold : 'float', optional
-            Minimun value to take into account in the creation of tauN
-            functions. For T>200 is convenient to set into 1.0e-14 to speed up
-            the calculations. The default is 0.0.
-        nphonon : 'int', optional
-            Phonon expansion order. The default is 1000.
-
-        Returns
-        -------
-        DDxs
-            Double differential cross section for elastic scattering
-
-        Examples
-        --------
-        # 0K xs data for U238:
-        >>> wd = os.getcwd()
-        >>> os.chdir(__file__.replace("ddxs.py", ""))
-        >>> os.chdir("../../data/xs/U238/")
-        >>> M = 238.05077040419212
-        >>> xs = Xs0K.from_file("u238.0.2", M)
-        >>> os.chdir(wd)
-
-        # Generate DDXS test variables:
-        >>> T = 1000
-        >>> Ein = 2.0
-        >>> Eout = np.array([1.8, 1.88008, 1.96016, 2.04024, 2.12032])
-        >>> theta = np.array([40, 80, 120, 160])
-        >>> from solid_cinel.tests.materials.UO2.examples import rho_in_energy_U238, interv_in_energy_U238
-        >>> pdos = Pdos.from_dE(rho_in_energy_U238, interv_in_energy_U238)
-
-        # S(alpha, -beta) algorithm for FGM:
-        >>> DDxs.from_Sab(xs, Ein, T, Eout, theta, model="fgm").data.round(6)
-        Eout        1.80000    1.88008    1.96016    2.04024   2.12032
-        mu
-        -0.939693  1.680633  11.840323  24.065568  15.217007  3.204407
-        -0.500000  0.656538   9.369964  26.822570  16.967671  2.590290
-         0.173648  0.018247   3.283892  32.168542  20.363097  0.977203
-         0.766044  0.000000   0.002708  26.843192  17.002521  0.001206
-
-        # S(alpha, -beta) algorithm for SCT:
-        >>> DDxs.from_Sab(xs, Ein, T, Eout, theta, pdos, model="sct").data.round(6)
-        Eout        1.80000    1.88008    1.96016    2.04024   2.12032
-        mu
-        -0.939693  1.693186  11.848713  24.024229  15.215447  3.221508
-        -0.500000  0.663895   9.388389  26.778357  16.967086  2.607195
-         0.173648  0.018701   3.305738  32.128467  20.370559  0.987929
-         0.766044  0.000000   0.002800  26.885282  17.056633  0.001251
-
-        # S(alpha, -beta) algorithm for PDOS:
-        >>> DDxs.from_Sab(xs, Ein, T, Eout, theta, pdos, threshold=1.0e-14, model="pdos").data.round(6)
-        Eout        1.80000    1.88008    1.96016    2.04024   2.12032
-        mu
-        -0.939693  1.724269  11.665751  24.157346  15.274187  3.160754
-        -0.500000  0.724235   9.192329  26.910247  17.022369  2.543125
-         0.173648  0.041094   3.335304  32.065389  20.299803  0.982933
-         0.766044  0.000016   0.040475  24.162304  15.423499  0.012977
-        """
-        # Get the Dynamic structure factor:
-        scatfunction = DynamicStruc.from_model(Ein, xs0K.M, T, Eout, theta, *args, **kwargs)
-
-        # Get the cross section in the correct energy grid:
-        xs0Kinterp = xs0K.interpolate(Eout, values=True)
-
-        return cls(xs0K, Ein, T, scatfunction.data * xs0Kinterp)
-
-    @classmethod
     def from_4PCF(cls, xs0K: Xs0K, Ein: float, T: float, Eout: np.ndarray,
-                  theta: np.ndarray, *args, algorithm: str = "sigma1",
+                  theta: np.ndarray, *args,
                   approx: bool = True, kind: str = "corrected", **kwargs):
         """
         Generate the Double Differential XS for elastic scattering from Fourier
@@ -334,8 +239,7 @@ class DDxs(DoubleDiffData):
         ..math::
             \frac{d^2\sigma_T(E)}{dE^\prime d^\theta} = \frac{1}{2 * k_B * T}\sqrt{\frac{E^\prime}{E}} S(\alpha(\theta, E^\prime, E, M, T), \beta( E^\prime, E, T)) \sigma^{T(1+\mu)/2}((E^\prime+E + \frac{\alpha k_{B} T}{1-\mu})/2 - E \mu / A)
 
-        For the xs matrix calculation, they are the following models available:
-            - "sigma1": sigma1 algorithm from NJOY2016 manual (default)
+        For the DSF, they are the following models available:
             - "fgm": Free Gas Model
             - "sct": Short Collision Time
             - "pdos": Phonon Density of States
@@ -352,11 +256,6 @@ class DDxs(DoubleDiffData):
             The neutron outgoing energy grid in eV
         theta : np.ndarray, (M,)
             The neutron outgoing angle grid in degrees (0, 180]
-        algorithm: str, optional
-            The algorithm use for getting the angle-integrated xs. The options
-            are:
-                - "sigma1" (default)
-                - "alpha0"
 
         Parameters for sct
         ------------------
@@ -441,8 +340,9 @@ class DDxs(DoubleDiffData):
                                                *args, **kwargs)
 
         # Get nuclear interaction parameters:
-        nuclearInteract = NucInteract.from_sigma(xs0K, Ein, T, Eout, theta, approx=approx,
-                                       kind=kind)
+        nuclearInteract = NucInteract.from_fgm(xs0K, Ein, T, Eout, theta,
+                                                 approx=approx,
+                                                 kind=kind)
 
         # Convolve the dynamic structure with the nuclear Interaction:
         return cls(xs0K, Ein, T,  dynamicStruc.data * nuclearInteract.data)
